2db369013fa37a4849e66ee69c183556d02db98a
mkarnam <mkarnam@MH23.corp.miraclesoft.com>
2019-04-30 22:10:33 +0530

    first maven simple project committed

---
+++ Sample/src/main/java/com/msc/App.java
@@ -0,0 +1,12 @@
+package com.msc;
+
+public class App {
+	
+ public static void main(String[] args) {
+	
+	 
+	 System.out.println("Hello Miracle");
+	 
+}
+
+}
---
+++ Sample/src/test/java/AppTest/AppTest.java
@@ -0,0 +1,25 @@
+package AppTest;
+
+//public class AppTest {
+	
+	import static org.junit.Assert.assertTrue;
+
+	import org.junit.Test;
+
+	/**
+	 * Unit test for simple App.
+	 */
+	public class AppTest 
+	{
+	    /**
+	     * Rigorous Test :-)
+	     */
+	    @Test
+	    public void shouldAnswerWithTrue()
+	    {
+	        assertTrue( true );
+	    }
+	}
+
+
+
